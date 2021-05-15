ae1982c7864a479f62871e0e0bd92cf07bb91965
bacaristic <medtaoufik.baccar@isticbc.org>
2021-05-15 20:44:26 +0200

    Fixed errors on login

--- app/src/main/java/com/example/myapplication/Login_Page.java
+++ app/src/main/java/com/example/myapplication/Login_Page.java
@@ -22,19 +22,10 @@ private ActivityLoginPageBinding binding;
      binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login_page);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
     @Override

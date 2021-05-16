ddb852e951bb64fad7742f5519e87b085faff283
Moe <moetez.jlidi@etu-univ.amu.fr>
2021-05-16 22:55:20 +0200

    Fixing a bug in Home page

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -28,18 +28,7 @@ public class User extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 }

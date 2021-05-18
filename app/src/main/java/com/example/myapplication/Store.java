ae22bd6cf7c8d6c7ed6812aab383bfc30b377702
Moe <moetez.jlidi@etu-univ.amu.fr>
2021-05-18 23:33:51 +0200

    Adding the offers and the bundle to the store

--- app/src/main/java/com/example/myapplication/Store.java
+++ app/src/main/java/com/example/myapplication/Store.java
@@ -18,6 +18,8 @@ public class Store extends AppCompatActivity {
         Picasso.get().load("https://media.valorant-api.com/weaponskinchromas/20ebb41a-4edc-0fa3-f3e9-b3b5d91e8524/displayicon.png").into((ImageView) findViewById(R.id.offer1));
         Picasso.get().load("https://media.valorant-api.com/weaponskins/9e648b20-4ed5-1f34-87a9-979cbe9a958a/displayicon.png").into((ImageView) findViewById(R.id.offer2));
         Picasso.get().load("https://media.valorant-api.com/weaponskins/57523cf0-4574-968b-9f17-168e3bdb6d0d/displayicon.png").into((ImageView) findViewById(R.id.offer3));
+        Picasso.get().load("https://media.valorant-api.com/weaponskins/65baa0cd-42ec-f99d-54a0-338d795b5824/displayicon.png").into((ImageView) findViewById(R.id.offer5));
+        //
         Picasso.get().load("https://media.valorant-api.com/weaponskins/65baa0cd-42ec-f99d-54a0-338d795b5824/displayicon.png").into((ImageView) findViewById(R.id.offer4));
         //
         //https://media.valorant-api.com/weaponskins/65baa0cd-42ec-f99d-54a0-338d795b5824/displayicon.png
